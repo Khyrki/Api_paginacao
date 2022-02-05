51a7f66b8a63114acba3943da4f64975fed8a8ee
Khyrki <khyrkimitsunobi@gmail.com>
2022-02-05 20:35:36 -0300

    Cria middleware de validacao dos inputs de pagina atula e total de paginas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser').json();
 const routerPaginacao = require('./routes/paginacao');
+const paginacaoValidation = require('./middleware/paginacaoValidation');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser);
 
-app.use('/paginacao', routerPaginacao);
+app.use('/paginacao', paginacaoValidation, routerPaginacao);
+
+app.use((err, req, res, _next) => {
+  if (err.isJoi) res.status(400).json({ message: err.details[0].message });
+  const internalError = 500;
+  res.status(err.code ? err.code : internalError).json({ message: err.message });
+});
 
 app.listen(3000, () => {
   console.log('O pai ta on!');
---
+++ src/middleware/paginacaoValidation.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+
+const validacoesAdicionais = ({ paginaAtual, paginasTotal }, next) => {
+  if (paginaAtual > paginasTotal) {
+    const err = {
+      code: 400,
+      message: 'paginaAtual cant be greater then paginasTotal',
+    };
+    next(err);
+  }
+};
+
+const paginacaoValidation = (req, res, next) => {
+  const { error } = Joi.object({
+    paginaAtual: Joi.number().integer().min(1).required(),
+    paginasTotal: Joi.number().integer().min(1).required(),
+  }).validate(req.body);
+
+  validacoesAdicionais(req.body, next);
+
+  if (error) next(error);
+
+  return next();
+};
+
+module.exports = paginacaoValidation;
